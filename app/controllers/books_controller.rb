c5a3c0f8cae0658720e9636b06e33df4c74da7f9
David McCown <dmccown@gmail.com>
2013-10-21 21:20:08 -0500

    Added ability to delete and edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,50 @@
 class BooksController < ApplicationController
-  def index 
+  def index
     @books = Book.paginate page: params[:page], per_page: 10
   end
 
-  def show 
+  def show
     @book = Book.find(params[:id])
-    @comments = @book.comments.paginate :page => params[:page], :per_page => 10, :order => 'created_at ASC'  
+    @comments = @book.comments.paginate :page => params[:page], :per_page => 10, :order => 'created_at ASC'
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new book_params
+    if @book.save
+      flash[:notice] = "#{@book.title} saved."
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find params[:id]
+  end
+
+  def update
+    @book = Book.find params[:id]
+    if @book.update_attributes book_params
+      flash[:notice] = "#{@book.title} saved."
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find params[:id]
+    book.destroy
+    flash[:notice] = "#{book.title} deleted."
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :thoughts)
   end
 end
